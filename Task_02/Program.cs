1cc99a301ff6244fc2c34be8d1fb1cf9d33c85ef
«isa» <isa.ilandaev@mail.ru>
2022-06-25 10:35:59 +0300

    Fix

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -4,9 +4,9 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.Write("Введите первое числа: ");
+Console.Write("Введите первое число: ");
 int numA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе числа: ");
+Console.Write("Введите второе число: ");
 int numB = Convert.ToInt32(Console.ReadLine());
 if (numA > numB)
 {
