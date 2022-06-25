89fa160ac88419d8d106221e1973d904a4e4bcb9
«isa» <isa.ilandaev@mail.ru>
2022-06-25 10:36:28 +0300

    Fix

--- Task_04/Program.cs
+++ Task_04/Program.cs
@@ -3,11 +3,11 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
-Console.Write("Введите первое числа: ");
+Console.Write("Введите первое число: ");
 int numA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе числа: ");
+Console.Write("Введите второе число: ");
 int numB = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите третье числа: ");
+Console.Write("Введите третье число: ");
 int numС = Convert.ToInt32(Console.ReadLine());
 
 int max = numA;
